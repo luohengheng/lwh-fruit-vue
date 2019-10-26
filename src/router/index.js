e53ce486830533ceb33523c6b6c61a946fd67a81
lwh <18311473541@163.com>
2019-10-26 21:31:43 +0800

    添加缩放

--- src/router/index.js
+++ src/router/index.js
@@ -1,15 +1,21 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
 import Login from '../views/Login.vue'
+import Home from '../views/Home.vue'
 
 Vue.use(VueRouter)
 
 const routes = [
-  {
-    path: '/login',
-    name: 'login',
-    component: Login
-  },
+    {
+      path: '/login',
+      name: 'login',
+      component: Login
+    },
+    {
+        path: '/home',
+        name: 'home',
+        component: Home
+    },
 ]
 
 const router = new VueRouter({
--- src/store/index.js
+++ src/store/index.js
@@ -5,8 +5,12 @@ Vue.use(Vuex)
 
 export default new Vuex.Store({
   state: {
+    isCollapseMenu: false,
   },
   mutations: {
+    HANDLER_COLL_MENU(state, flag) {
+        state.isCollapseMenu = flag
+    }
   },
   actions: {
   },
